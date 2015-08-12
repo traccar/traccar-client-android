fe9132e46a17ea4516d11c8af59e2ff1aaa04bb2
Anton Tananaev <anton.tananaev@gmail.com>
2015-08-12 20:32:42 +1200

    Remove old unused code

--- app/src/main/java/org/traccar/client/ProtocolFormatter.java
+++ app/src/main/java/org/traccar/client/ProtocolFormatter.java
@@ -15,11 +15,6 @@
  */
 package org.traccar.client;
 
-import java.util.Calendar;
-import java.util.Formatter;
-import java.util.Locale;
-import java.util.TimeZone;
-
 import android.location.Location;
 import android.net.Uri;
 
@@ -41,53 +36,5 @@ public class ProtocolFormatter {
         return builder.build().toString();
     }
 
-    /**
-     * Format device id message
-     */
-    public static String createLoginMessage(String id) {
-        StringBuilder s = new StringBuilder("$PGID,");
-        Formatter f = new Formatter(s, Locale.ENGLISH);
-
-        s.append(id);
-
-        byte checksum = 0;
-        for (byte b : s.substring(1).getBytes()) {
-            checksum ^= b;
-        }
-        f.format("*%02x\r\n", (int) checksum);
-        f.close();
-
-        return s.toString();
-    }
-
-    /**
-     * Format location message
-     */
-    public static String createLocationMessage(Location l, double battery) {
-        StringBuilder s = new StringBuilder("$GPRMC,");
-        Formatter f = new Formatter(s, Locale.ENGLISH);
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"), Locale.ENGLISH);
-        calendar.setTimeInMillis(l.getTime());
-
-        f.format("%1$tH%1$tM%1$tS.%1$tL,A,", calendar);
-
-        double lat = l.getLatitude();
-        double lon = l.getLongitude();
-        f.format("%02d%07.4f,%c,", (int) Math.abs(lat), Math.abs(lat) % 1 * 60, lat < 0 ? 'S' : 'N');
-        f.format("%03d%07.4f,%c,", (int) Math.abs(lon), Math.abs(lon) % 1 * 60, lon < 0 ? 'W' : 'E');
-
-        double speed = l.getSpeed() * 1.943844; // speed in knots
-        f.format("%.2f,%.2f,", speed, l.getBearing());
-        f.format("%1$td%1$tm%1$ty,,", calendar);
-
-        byte checksum = 0;
-        for (byte b : s.substring(1).getBytes()) {
-            checksum ^= b;
-        }
-        f.format("*%02x\r\n", (int) checksum);
-        f.close();
-
-        return s.toString();
-    }
 }
 
