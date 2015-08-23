5f7a1a8bfd3516ce484f60e4a2c98c0be6f99cde
Anton Tananaev <anton.tananaev@gmail.com>
2015-08-23 20:11:39 +1200

    Use single quotes in gradle

--- app/src/main/java/org/traccar/client/TrackingService.java
+++ app/src/main/java/org/traccar/client/TrackingService.java
@@ -76,7 +76,6 @@ public class TrackingService extends Service {
         if (intent != null) {
             AutostartReceiver.completeWakefulIntent(intent);
         }
-
     }
 
     @TargetApi(Build.VERSION_CODES.ECLAIR)
