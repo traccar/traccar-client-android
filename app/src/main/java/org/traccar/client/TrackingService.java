efdaf69d0728e2dc4041c2d90bb2f0ea237c0982
Anton Tananaev <anton.tananaev@gmail.com>
2016-09-01 02:28:35 +1200

    Remove unused class import

--- app/src/main/java/org/traccar/client/TrackingService.java
+++ app/src/main/java/org/traccar/client/TrackingService.java
@@ -23,7 +23,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.lang.reflect.InvocationTargetException;
