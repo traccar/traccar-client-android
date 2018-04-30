bcde8388d642134833a1365ce9db99b5c872576f
Anton Tananaev <anton.tananaev@gmail.com>
2018-05-01 10:01:57 +1200

    Update database version

--- app/src/main/java/org/traccar/client/DatabaseHelper.java
+++ app/src/main/java/org/traccar/client/DatabaseHelper.java
@@ -27,7 +27,7 @@ import java.util.Date;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "traccar.db";
 
     public interface DatabaseHandler<T> {
