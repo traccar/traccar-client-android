9360ed26ff9d8a7dfe9c65d01b146445e48a0d66
Anton-V-K <20116984+Anton-V-K@users.noreply.github.com>
2022-01-29 21:44:22 +0300

    Status Widget #429

--- app/src/main/java/org/traccar/client/StatusWidget.kt
+++ app/src/main/java/org/traccar/client/StatusWidget.kt
@@ -63,21 +63,20 @@ class StatusWidget : AppWidgetProvider() {
             update(context, manager, appWidgetIds)
         }
 
+        // Performs update for the widgets with given identifiers.
+        internal fun update(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+            val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+            val enabled = prefs.getBoolean(MainFragment.KEY_STATUS, false)
+            // There may be multiple widgets active, so update all of them
+            for (appWidgetId in appWidgetIds) {
+                // Construct the RemoteViews object
+                val views = RemoteViews(context.packageName, R.layout.status_widget)
+                views.setImageViewResource(R.id.ivEnabled, if (enabled) R.mipmap.ic_start else R.mipmap.ic_stop)
+
+                // Instruct the widget manager to update the widget
+                appWidgetManager.updateAppWidget(appWidgetId, views)
+            }
+        }
     }
 
-}
-
-// Performs update for the widgets with given identifiers.
-internal fun update(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
-    val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-    val enabled = prefs.getBoolean(MainFragment.KEY_STATUS, false)
-    // There may be multiple widgets active, so update all of them
-    for (appWidgetId in appWidgetIds) {
-        // Construct the RemoteViews object
-        val views = RemoteViews(context.packageName, R.layout.status_widget)
-        views.setImageViewResource(R.id.ivEnabled, if (enabled) R.mipmap.ic_start else R.mipmap.ic_stop)
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views)
-    }
 }
