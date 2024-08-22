1d1f1bd99b3a01c39460481fd1e588767156a5c4
dw <dw@ednt.de>
2024-08-22 16:04:22 +0200

    Atlas Branding

---
+++ app/src/test/java/de/ednt/altasclient/ProtocolFormatterTest.kt
@@ -0,0 +1,39 @@
+package de.ednt.altasclient
+
+import android.location.Location
+import android.os.Build
+import de.ednt.atlasclient.BatteryStatus
+import de.ednt.atlasclient.Position
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import de.ednt.atlasclient.ProtocolFormatter.formatRequest
+
+@Config(sdk = [Build.VERSION_CODES.P])
+@RunWith(RobolectricTestRunner::class)
+class ProtocolFormatterTest {
+
+    @Test
+    fun testFormatRequest() {
+        val position = Position("123456789012345", Location("gps"), BatteryStatus())
+        val url = formatRequest("http://localhost:5055", position)
+        Assert.assertEquals("http://localhost:5055?id=123456789012345&timestamp=619315200&lat=0.0&lon=0.0&speed=0.0&bearing=0.0&altitude=0.0&accuracy=0.0&batt=0.0", url)
+    }
+
+    @Test
+    fun testFormatPathPortRequest() {
+        val position = Position("123456789012345", Location("gps"), BatteryStatus())
+        val url = formatRequest("http://localhost:8888/path", position)
+        Assert.assertEquals("http://localhost:8888/path?id=123456789012345&timestamp=619315200&lat=0.0&lon=0.0&speed=0.0&bearing=0.0&altitude=0.0&accuracy=0.0&batt=0.0", url)
+    }
+
+    @Test
+    fun testFormatAlarmRequest() {
+        val position = Position("123456789012345", Location("gps"), BatteryStatus())
+        val url = formatRequest("http://localhost:5055/path", position, "alert message")
+        Assert.assertEquals("http://localhost:5055/path?id=123456789012345&timestamp=619315200&lat=0.0&lon=0.0&speed=0.0&bearing=0.0&altitude=0.0&accuracy=0.0&batt=0.0&alarm=alert%20message", url)
+    }
+
+}
---
+++ app/src/test/java/de/ednt/altasclient/RequestManagerTest.kt
@@ -0,0 +1,22 @@
+package de.ednt.altasclient
+
+import android.os.Build
+import org.junit.Assert
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import de.ednt.atlasclient.RequestManager.sendRequest
+
+@Config(sdk = [Build.VERSION_CODES.P])
+@RunWith(RobolectricTestRunner::class)
+class RequestManagerTest {
+
+    @Ignore("Not a real unit test")
+    @Test
+    fun testSendRequest() {
+        Assert.assertTrue(sendRequest("http://www.google.com"))
+    }
+
+}
